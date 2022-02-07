25e97a7ce3d24e0e9d7853fea89edefc778d615b
elroeyayiso <86745767+elroeyayiso@users.noreply.github.com>
2022-02-06 17:45:35 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,167 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <menta name= "viewport"
+   content= "width= device-width,
+   initial-scale=1.0"/>
+   <meta http-equiv="X-UA-Compatible" 
+   content="ie=edge">
+      <title>Portal page</title>
+    <link href="css/styles.css" type="text/css" rel= "stylesheet">
+      
+</head>
+<body>
+<header>
+<div id= "inner-header">
+ <h1 id= "Portal"><a href= "indext.php"> My Portal page</a></h1>
+</h1>
+
+    <nav>
+        <ul>
+            <li><a href= "index.php"> Home</a></li>
+            <li><a href= "Switch.php"> Switch-3</a></li>
+            <li><a href= "Troubleshoot.php"> Troubleshoot-4</a></li>
+            <li><a href= "Calcultor.php"> Calcultor</a></li>
+            <li><a href= "Email.php"> Email</a></li>
+            <li><a href= "Gallery.php"> Gallery</a></li>
+
+           
+</ul>
+</nav>
+</div>
+        
+<!-- end inner header-->
+</header>
+
+<div id= "wrapper">
+<div id= "hero">
+    
+    
+<p>
+    Hi I'm Elroe Yayiso. I was born and raised in Ethiopia.
+</p>
+    <p>
+    I'm a senoir at Roosevlt high school and running start student at Seattle central Collge.
+</p>
+    <p>
+    I moved here when i was 11 years old. My hobbies are taking pictures,watching tiktok, and cooking.
+</p>
+<p>
+    I'm planning on majoring in computer science.
+</p> 
+<img src="Image/Elroe photo.png" alt="photo" width="200" height="220">
+
+   
+</div> <!--end hero-->
+
+ <main>
+    <img src="Image/Screen Shot .png" alt="photo" width="400" height="500">
+
+
+    
+ </main>
+
+<aside>
+    Class Exerises
+<p>Week 1:
+(No Homework)
+</p>
+
+<p>Week 2:</p>
+<p>
+    <a href="http://localhost:8888/IT261/Weeks/Week%202/var.php">var.php</a>
+</p>
+
+<p>
+    <a href="http://localhost:8888/IT261/Weeks/Week%202/currency.php">currency.php</a>
+</p>
+
+<p>
+    <a href="http://localhost:8888/IT261/Weeks/Week%202/currency logic.php">currency logic.php</a>
+</p>
+
+<p>
+    <a href="http://localhost:8888/IT261/Weeks/Week%202/heredoc.php">heredoc.php</a>
+</p>
+
+<p>Week 3:</p>
+<p>
+<a href="http://localhost:8888/IT261/Weeks/Week%203/If-else.php">If-else.php</a>
+</p>
+<p>
+
+    <a href="http://localhost:8888/IT261/Weeks/Week%203/arrays.php">arrays</a>
+</p>
+<p>
+    <a href="http://localhost:8888/IT261/Weeks/Week%203/date.php">date.php</a>
+    <p>
+        <a href="http://localhost:8888/IT261/Weeks/Week%203/for-loop.php">for-loop.php</a>
+    </p>
+    <p>
+        <a href="http://localhost:8888/IT261/Weeks/Week%203/switch.php">switch.php</a>
+    </p>
+
+<p>Week 4</p>
+
+<p>
+    <a href="http://localhost:8888/IT261/Weeks/Week%204/form-get.php">form-get.php</a>
+</p>
+
+
+<p>
+    <a href="http://localhost:8888/IT261/Weeks/Week%204/form1.php"> form1.php</a>
+</p>
+
+
+<p>
+    <a href="http://localhost:8888/IT261/Weeks/Week%204/form2.php">form2.php</a>
+</p>
+
+
+<p>
+    <a href="http://localhost:8888/IT261/Weeks/Week%204/form3.php">form3.php</a>
+</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</p>
+</aside>
+
+</div> <!--end wrapper-->
+
+<footer>
+    <div id= "inner-footer">
+<ul>
+    <li>copyright &copy;</li>
+    <li>All Rights Reserved;</li>
+    <li><a href= "../index.php"> Web Design By Elroe</a></li>
+
+<li><a href= "">HTML Validation</a></li>
+<li><a href= "">CSS Validation</a></li>
+</ul>
+</div> <!--end inner footer-->
+</footer>
+
+</body>
+</html>
+
+
+
+
+
+
+
